73d5d4510c42d36a53d65d7f68c35b4c25bfea03
Krutik Patel <krutikmpatel@gmail.com>
2023-07-02 01:07:09 +0530

    added recent files

--- src/Chat.java
+++ src/Chat.java
@@ -4,131 +4,208 @@ import javax.swing.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.io.*;
 import javax.swing.border.*;
+
+import javafx.scene.layout.Border;
+
 import java.util.ArrayList;
 
 public class Chat implements ActionListener {
     private JFrame frame;
-    private JPanel textPanel, sendPanel, topPanel;
+    private JPanel mainPanel, textPanel, sendPanel, topPanel;
     private String chatBoxTitle = "ChatBox";
     private JTextField messageField;
+    private JScrollPane scrollPane;
     private JButton sendButton;
     private ArrayList<MessageItem> msgList;
-    private Box box = Box.createVerticalBox();
 
-    public Chat() {
+    public enum Author {
+        USER,
+        GRETA
+    }
+
+    public void init() {
+        new Chat();
+    }
+
+    private Chat() {
         msgList = new ArrayList<>();
 
         frame = new JFrame();
+        mainPanel = new JPanel();
+
         frame.setResizable(false);
         frame.setSize(1000, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(chatBoxTitle);
+        frame.setLocationRelativeTo(null);
+        mainPanel.setLayout(new BorderLayout());
 
-        textPanel = new JPanel();
-        textPanel.setLayout(null);
-        textPanel.setBounds(0, 80, 1000, 700);
-        textPanel.setBackground(Color.decode("#F3FFFD"));
-        JScrollPane textScrollPane = new JScrollPane(textPanel);
+        addTextPanel();
+        addSendPanel();
+        addTopPanel();
 
-        
-        topPanel = new JPanel();
-        topPanel.setLayout(null);
-        topPanel.setBounds(0, 0, 1000, 80);
-        topPanel.setBackground(Color.DARK_GRAY);
-        JLabel greta = new JLabel("GRETA");
-        greta.setFont(new Font("SANS-SERIF", Font.BOLD, 24));
-        greta.setBounds(10, 18, 100, 50);
-        greta.setForeground(Color.WHITE);
-        topPanel.add(greta);
-        
-        sendPanel = new JPanel();
-        sendPanel.setLayout(null);
-        sendPanel.setBackground(Color.decode("#ADC5E0"));
-        sendPanel.setBounds(0, 705, 1000, 60);
-        
-        frame.add(sendPanel);
-        frame.add(textPanel);
-        frame.add(topPanel);
-        
-        addSendEvent();
+        frame.add(mainPanel);
 
         frame.setVisible(true);
 
     }
 
-    private void addSendEvent() {
+    private void addTextPanel() {
+        textPanel = new JPanel();
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+        textPanel.setBackground(Color.decode("#E2E2E2"));
+        textPanel.setBorder(new EmptyBorder(5, 10, 5, 10));
 
-        messageField = new JTextField(80);
-        messageField.setBounds(8, 8, 860, 46);
+        scrollPane = new JScrollPane(textPanel);
+        scrollPane.setBorder(null);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void addTopPanel() {
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("./images/Usr.png"));
+        Image i2 = i1.getImage().getScaledInstance(42, 42, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+        topPanel.setBorder(new EmptyBorder(5, 10, 5, 10));
+        topPanel.setBackground(Color.decode("#255c99"));
+        JLabel greta = new JLabel("GRETA", i3, SwingConstants.LEADING);
+        greta.setFont(new Font("SANS-SERIF", Font.BOLD, 24));
+        greta.setForeground(Color.WHITE);
+        topPanel.add(greta, BorderLayout.CENTER);
+        topPanel.setPreferredSize(new Dimension(1000, 80));
+
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+    }
+
+    private void addSendPanel() {
+        sendPanel = new JPanel();
+        sendPanel.setLayout(new BorderLayout());
+        sendPanel.setBackground(Color.decode("#ADC5E0"));
+        sendPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.decode("#ACACAC")));
+        sendPanel.setPreferredSize(new Dimension(1000, 60));
+
+        addSendButton();
+
+        mainPanel.add(sendPanel, BorderLayout.SOUTH);
+    }
+
+    private void addSendButton() {
+
+        messageField = new JTextField();
         messageField.setFont(new Font("SANS-SERIF", Font.PLAIN, 20));
-        messageField.setBorder(new EmptyBorder(0, 10, 0, 0));
-        sendPanel.add(messageField);
+        messageField.setText("Message");
+        messageField.setForeground(Color.GRAY);
+        messageField.setPreferredSize(new Dimension(875, 60));
+        messageField.setBorder(new EmptyBorder(0, 15, 0, 15));
+        messageField.addKeyListener(new KeyListener() {
+            private boolean noText = true;
+
+            public void keyPressed(KeyEvent k) {
+            }
+
+            public void keyTyped(KeyEvent k) {
+                if (messageField.getText().equals("")) {
+                    messageField.setText("Message");
+                    messageField.setForeground(Color.GRAY);
+                    messageField.setCaretPosition(0);
+                    noText = true;
+                } else if (noText && !k.isActionKey()) {
+                    noText = false;
+                    messageField.setText("");
+                    messageField.setForeground(Color.BLACK);
+                }
+            }
+
+            public void keyReleased(KeyEvent k) {
+            }
+        });
+        sendPanel.add(messageField, BorderLayout.WEST);
 
         sendButton = new JButton("Send");
         sendButton.setFont(new Font("SANS-SERIF", Font.BOLD, 20));
-
-        sendButton.setBounds(875, 0, 125, 60);
         sendButton.setBackground(Color.decode("#255c99"));
         sendButton.setForeground(Color.WHITE);
+        sendButton.setBorder(null);
         frame.getRootPane().setDefaultButton(sendButton);
         sendButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
                 String msg = messageField.getText();
-                if (msg.equals("")) { return; }
+                if (msg.equals("")) {
+                    return;
+                }
                 System.out.println(msg);
-                JPanel newMsg = addToList(msg, true, e);
-                textPanel.setLayout(new BorderLayout());
-                
-                JPanel right = new JPanel(new BorderLayout());
 
-                right.add(newMsg, BorderLayout.LINE_END);
-                right.setBackground(Color.decode("#F3FFFD"));
-                box.add(right);
-                box.add(Box.createRigidArea(new Dimension(0, 10)));
-                textPanel.add(box, BorderLayout.PAGE_START);
+                Box newMsgArea = createMessageArea(msg, Author.USER, e);
+                textPanel.add(newMsgArea);
                 frame.validate();
+                Box newMsgArea1 = createMessageArea(msg, Author.GRETA, e);
+                textPanel.add(newMsgArea1);
+                frame.validate();
+                scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
 
                 messageField.setText("");
             }
         });
-        
-        sendPanel.add(sendButton);
+
+        sendButton.setPreferredSize(new Dimension(125, 60));
+        sendPanel.add(sendButton, BorderLayout.EAST);
 
     }
-    
-    private JPanel addToList(String msg, boolean fromUser, ActionEvent e) {
-        Color textAreaColor, foregroundColor;
-        if (fromUser) {
-            textAreaColor = Color.decode("#255c99");
-            foregroundColor = Color.WHITE;
-        } else {
-            textAreaColor = Color.WHITE;
-            foregroundColor = Color.BLACK;
+
+    private Box createMessageArea(String msg, Author author, ActionEvent e) {
+        Color textAreaColour = Color.WHITE, foregroundColour = Color.WHITE;
+        float textAlignment = Component.LEFT_ALIGNMENT;
+        if (author == Author.USER) {
+            textAreaColour = Color.decode("#255c99");
+            foregroundColour = Color.WHITE;
+            textAlignment = Component.RIGHT_ALIGNMENT;
+
+        } else if (author == Author.GRETA) {
+            textAreaColour = Color.WHITE;
+            foregroundColour = Color.BLACK;
+            textAlignment = Component.LEFT_ALIGNMENT;
         }
-        
-        msgList.add(new MessageItem(msg, fromUser));
-        JPanel textArea = new JPanel();
-        textArea.setLayout(new BoxLayout(textArea, BoxLayout.Y_AXIS));
-        textArea.setBackground(Color.decode("#F3FFFD"));
 
-        JLabel __label = new JLabel(msg);
-        __label.setFont(new Font("SANS-SERIF", Font.PLAIN, 18));
-        __label.setBackground(textAreaColor);
-        __label.setOpaque(true);
-        __label.setForeground(foregroundColor);
-        __label.setBorder(new EmptyBorder(5, 10, 5, 10));
+        JPanel msgArea = new JPanel();
+        msgArea.setLayout(new BoxLayout(msgArea, BoxLayout.Y_AXIS));
+        msgArea.setBackground(null);
 
+        JLabel msgText = new JLabel(msg);
+        msgText.setBackground(textAreaColour);
+        msgText.setForeground(foregroundColour);
+        msgText.setOpaque(true);
+        msgText.setBorder(new EmptyBorder(5, 10, 5, 10));
+        msgText.setFont(new Font("SANS-SERIF", Font.PLAIN, 18));
+        msgText.setAlignmentX(textAlignment);
+
+        JLabel timeLabel = new JLabel();
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat time = new SimpleDateFormat("HH:MM");
+        Date msgTime = cal.getTime();
+        timeLabel.setText(time.format(msgTime));
+        timeLabel.setAlignmentX(textAlignment);
+        msgList.add(new MessageItem(msg, Author.USER, msgTime));
 
-        JLabel timeLabel = new JLabel(time.format(cal.getTime()));
-        textArea.add(__label);
-        textArea.add(timeLabel);
+        msgArea.add(msgText);
+        msgArea.add(timeLabel);
+        msgArea.add(Box.createVerticalStrut(10));
+        msgArea.add(Box.createHorizontalGlue());
+        return justify(msgArea, textAlignment);
+    }
 
-        return textArea;
+    private Box justify(JPanel panel, float alignment) {
+        Box b = Box.createHorizontalBox();
+        b.add(panel);
+        if (alignment == Component.LEFT_ALIGNMENT) {
+            b.add(Box.createHorizontalGlue());
+        }
+        return b;
     }
 
     public static void main(String[] args) throws Exception {
@@ -141,26 +218,24 @@ public class Chat implements ActionListener {
 
 class MessageItem {
     private String messageText;
-    private boolean fromUser;
+    private Chat.Author author;
+    private Date time;
 
-    public MessageItem(String msg, boolean fromUser) {
+    public MessageItem(String msg, Chat.Author author, Date time) {
         this.messageText = msg;
-        this.fromUser = fromUser;
+        this.author = author;
+        this.time = time;
     }
 
     public String getMessageText() {
         return messageText;
     }
 
-    public void setMessageText(String msg) {
-        this.messageText = msg;
-    };
-
-    public boolean isFromUser() {
-        return fromUser;
+    public Chat.Author isFromUser() {
+        return author;
     }
 
-    public void setFromUser(boolean fromUsr) {
-        this.fromUser = fromUsr;
+    public Date getTime() {
+        return time;
     }
 }
